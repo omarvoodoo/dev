565d25e253726ff2d899131ca281707198dde17e
Yungyikim <yungyikim@gmail.com>
2015-10-19 08:14:43 +0000

    update

--- Go/Packages/http/http.go
+++ Go/Packages/http/http.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"net"
 	"net/http"
 )
 
+const helloWorldString = "Hello, World!"
+
+var (
+	helloWorldBytes = []byte(helloWorldString)
+)
+
 func main() {
-	var listener net.Listener
-	flag.Parse()
+	http.HandleFunc("/text", textHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func textHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write(helloWorldBytes)
 }
