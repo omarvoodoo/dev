a974ea209bcca8980712fc8b4072a5177a33c16e
yungyikim <yungyikim@gmail.com>
2015-10-21 18:07:02 +0900

    update

--- Go/Packages/http/http.go
+++ Go/Packages/http/http.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"runtime"
 )
@@ -15,10 +17,31 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	http.HandleFunc("/text", textHandler)
+	http.HandleFunc("/json", jsonHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func textHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write(helloWorldBytes)
+}
+
+type joinData struct {
+	Test string
+}
+
+// Test it with: curl -X POST -d "{\"test\": \"that\"}" http://localhost:8080/json
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	// Body 구문을 분석
+	decoder := json.NewDecoder(r.Body)
+	t := new(joinData)
+	err := decoder.Decode(&t)
+	checkErr(err, "Decode failed")
+	log.Println(t.Test)
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
