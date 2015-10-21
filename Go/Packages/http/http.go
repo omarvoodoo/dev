77342029e0b9375988a426b763c014f2b5a5ea2a
yungyikim <yungyikim@gmail.com>
2015-10-21 18:44:37 +0900

    update

--- Go/Packages/http/http.go
+++ Go/Packages/http/http.go
@@ -26,18 +26,27 @@ func textHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(helloWorldBytes)
 }
 
-type joinData struct {
+type joinReqData struct {
 	Test string
+}
+
+type joinRspData struct {
+	Result string
 }
 
 // Test it with: curl -X POST -d "{\"test\": \"that\"}" http://localhost:8080/json
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	// Body 구문을 분석
-	decoder := json.NewDecoder(r.Body)
-	t := new(joinData)
-	err := decoder.Decode(&t)
+	reqBody := new(joinReqData)
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
 	checkErr(err, "Decode failed")
-	log.Println(t.Test)
+	log.Println(reqBody.Test)
+
+	// json형식의 응답 메세지 구성
+	rspBody := joinRspData{
+		Result: "result hahahaha",
+	}
+	json.NewEncoder(w).Encode(rspBody)
 }
 
 func checkErr(err error, msg string) {
